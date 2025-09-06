85ea28d5a6e1fc990672198f290fac3a5eaab101
Eduardo Filho <eduardosmfilho01@gmail.com>
2025-09-06 02:03:47 +0100

    Configurando navbar.

--- vendas/views.py
+++ vendas/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+#from django.http import HttpResponse
+from . import forms
 
-# Create your views here.
+def index(request):
+    return render(request, "vendas/index.html")
+
+#.................... CONTA ..................
+def login(request):
+    return render(request, "vendas/conta/login.html")
+
+def cadastro(request):
+    return render(request, "vendas/conta/cadastro.html")
+#....................................................
+
+#.................... NAVBAR LINKS ..................
+def lojas(request):
+    return render(request, "vendas/navbar/lojas.html")
+
+def computadores(request):
+    return render(request, "vendas/navbar/computadores.html")
+
+def tablets(request):
+    return render(request, "vendas/navbar/tablets.html")
+
+def drones_camera(request):
+    return render(request, "vendas/navbar/drones_camera.html")
+
+def audio(request):
+    return render(request, "vendas/navbar/audio.html")
+
+def mobile(request):
+    return render(request, "vendas/navbar/mobile.html")
+
+def tv_home_theater(request):
+    return render(request, "vendas/navbar/tv_home_theater.html")
+
+def tec_vestivel(request):
+    return render(request, "vendas/navbar/tec_vestivel.html")
+
+def promocoes(request):
+    return render(request, "vendas/navbar/promocoes.html")
+#....................................................
